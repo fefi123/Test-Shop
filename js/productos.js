fbd5cfddf89b72687dab5d8287b40d7fbd7ecb6d
fefi <fe_de.kpo2012@hotmail.com>
2023-11-27 17:46:31 -0300

    add json

---
+++ js/productos.js
@@ -0,0 +1,21 @@
+let productos = [];
+let productosMain = [];
+
+// cargar json de productosMain
+
+async function cargarProductosMain() {
+  const response = await fetch();
+}
+
+// function listadoDeProductos(array) {
+//   let productos = document.getElementById("productos");
+//   productos.innerHTML = "";
+//   array.forEach((producto) => {
+//     productos.innerHTML += `<article class='card'>
+//       <img src='${producto.imagen}'/>
+//       <h3>${producto.nombre}</h3>
+//       <p>US$ ${producto.precio}.00</p>
+//       <button class="btnAgregar" data-id="${producto.id}"><i class="bi bi-cart-plus-fill"></i></button>
+//       </article>`;
+//   });
+// }
