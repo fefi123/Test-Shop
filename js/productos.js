051dcc77630b92c310489104c484ff0900dbb724
fefi <fe_de.kpo2012@hotmail.com>
2023-12-08 18:21:01 -0300

    add img

--- js/productos.js
+++ js/productos.js
@@ -1,21 +1,47 @@
 let productos = [];
-let productosMain = [];
 
-// cargar json de productosMain
+// Carga todos los productos
 
-async function cargarProductosMain() {
-  const response = await fetch();
+async function cargarProductos() {
+  const response = await fetch(
+    `https://fefi123.github.io/Test-Shop/productosMain.json`
+  );
+  productos = await response.json();
+  listadoDeProductos(productos);
 }
 
-// function listadoDeProductos(array) {
-//   let productos = document.getElementById("productos");
-//   productos.innerHTML = "";
-//   array.forEach((producto) => {
-//     productos.innerHTML += `<article class='card'>
-//       <img src='${producto.imagen}'/>
-//       <h3>${producto.nombre}</h3>
-//       <p>US$ ${producto.precio}.00</p>
-//       <button class="btnAgregar" data-id="${producto.id}"><i class="bi bi-cart-plus-fill"></i></button>
-//       </article>`;
-//   });
-// }
+//productos del inicio
+
+function listadoDeProductos(array) {
+  let productos = document.getElementById("productos");
+  productos.innerHTML = "";
+  array.forEach((producto) => {
+    productos.innerHTML += `<article class='card'>
+    <img src='${producto.imagen}'/>
+    <div class="card-info">
+      <h3>${producto.nombre}</h3>
+    <div style="display:flex">
+      <p>$ ${producto.precio}.00</p>
+      <button class="btnAdd" data-id="${producto.id}"><svg xmlns="http://www.w3.org/2000/svg" height="1.5em" viewBox="0 -100 600 600"><path d="M0 24C0 10.7 10.7 0 24 0H69.5c22 0 41.5 12.8 50.6 32h411c26.3 0 45.5 25 38.6 50.4l-41 152.3c-8.5 31.4-37 53.3-69.5 53.3H170.7l5.4 28.5c2.2 11.3 12.1 19.5 23.6 19.5H488c13.3 0 24 10.7 24 24s-10.7 24-24 24H199.7c-34.6 0-64.3-24.6-70.7-58.5L77.4 54.5c-.7-3.8-4-6.5-7.9-6.5H24C10.7 48 0 37.3 0 24zM128 464a48 48 0 1 1 96 0 48 48 0 1 1 -96 0zm336-48a48 48 0 1 1 0 96 48 48 0 1 1 0-96z"/></svg></button>
+    </div>
+    </div>
+    </article>`;
+  });
+}
+cargarProductos();
+
+// btn que agrega productos al carrito
+const botonesAgregar = document.getElementsByClassName("btnAdd");
+for (const boton of botonesAgregar) {
+  boton.addEventListener("click", (event) => {
+    event.preventDefault();
+    const id = Number(boton.dataset.id);
+    agregarProducto(Number(boton.dataset.id));
+  });
+}
+
+// funcion para agregar productos al carrito
+
+function returnProducto(id) {
+  return productos.find((producto) => producto.id === id);
+}
